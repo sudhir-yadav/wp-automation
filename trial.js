05b2a2eb4a267af53734dc61de16989ee2e6eefd
sudhir-yadav <sudhir.yadav@rtcamp.com>
2020-01-30 14:34:01 +0530

    Init

---
+++ trial.js
@@ -0,0 +1,49 @@
+/**
+ * Sudhir Yadav 
+ * 28th jan 2020
+ * WP Automation
+ */
+
+const pw     = require('playwright');
+const dotenv = require('dotenv').config();
+
+(async () => {
+  const browser = await pw.webkit.launch( { headless: false } ); // or 'chromium', 'firefox'
+  //const browser = await pw.webkit.launch(); // or 'chromium', 'firefox'
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.setViewport({ width: 1280, height: 800 })
+
+  const url = process.env.WP_URL ; 
+
+  if( 'true' === process.env.HTTP_AUTH ){
+      url = (process.env.HTTP_AUTH_USER + ':'+process.env.HTTP_AUTH_PASS)+url;
+  }
+
+  await page.goto( 'https://'+url+'/wp-admin/' );
+
+  const userField   = await page.$('#user_login');
+  const passField   = await page.$('#user_pass');
+  const loginButton = await page.$('#wp-submit');
+
+  await userField.fill( 'mldemo' );
+  await passField.type( 'mldemo#123');
+  await page.screenshot( { path: 'userpassword.png' } );
+  await loginButton.click('button', {delay: 500});
+  await page.waitForNavigation();
+  
+  await page.screenshot({ path: 'wp-admin.png' }); 
+
+  const create_post = await page.$( '#menu-posts' );
+  create_post.click();
+  await page.screenshot({ path: 'wp-admin.png' }); 
+
+  await Promise.all([
+    await page.click( '.page-title-action' )
+  ]);
+  await page.waitForNavigation();
+  await page.screenshot({ path: 'app.png' }); 
+  //await page.evaluate(() => { document.querySelector('.page-title-action')[1].click(); });
+
+  await browser.close( {delay: 2000} );
+})();
